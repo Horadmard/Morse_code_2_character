10add1edda8a0cee372feea031601238b0b8e378
Hossein Radmard <124653035+Horadmard@users.noreply.github.com>
2024-03-11 23:43:40 +0330

    Update and rename MTS.c to src/MTS.c

--- src/MTS.c
+++ src/MTS.c
@@ -2,7 +2,7 @@
 This program was created by the
 CodeWizardAVR V3.12 Advanced
 Automatic Program Generator
-© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+Â© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
 http://www.hpinfotech.com
 
 Project : 
